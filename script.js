f7cd8f28bb493a892cbe08af113e8ab9c6a32e1b
ebrahim <ebrahimasil@gmail.com>
2025-04-11 10:38:08 +0430

    script:  implement image switcher on product page

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ if (close) {
     navbar.classList.remove("active");
   });
 }
+
+// product images preview
+var MainImg = document.getElementById("MainImg");
+var smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = function () {
+  MainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  MainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  MainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  MainImg.src = smallImg[3].src;
+};
